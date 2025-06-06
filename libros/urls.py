f037c1e31e0c3c9f5a7e3b9d0bb0b6d0c0275d5c
carcangar02 <carloscantosgarcia02@gmail.com>
2025-06-06 02:05:10 +0200

    vista menu Ajax libros ya cargados, filtro en cliente

--- libros/urls.py
+++ libros/urls.py
@@ -5,7 +5,6 @@ app_name = 'carcanbooks'  # para usar namespace en templates o reverses
 
 urlpatterns = [
     path('', views.librerias_menu, name='librerias_menu'),
-    path('libreria/', views.cargar_libros_por_libreria, name='carga_dinamica_menu'),
     # path('libro/<str:libro.titulo>/', views.libro_details, name='libro_details'),
 
 ]
--- libros/views.py
+++ libros/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Libreria, Libro
 import importlib
+import json
 
 def librerias_menu(request):
     librerias = Libreria.objects.values('pk', 'nombre')
-    libreria_seleccionada = request.GET.get('libreria')  # id seleccionada desde el GET
 
     info_libros = []
-    libros_qs = Libro.objects.filter(libreria_id=1)
+    libros_qs = Libro.objects.all()
 
     # Si hay una librería seleccionada, filtra por ella
 
@@ -28,28 +28,9 @@ def librerias_menu(request):
             print(e)
 
     context = {
-        'librerias': librerias,
-        'info_libros': info_libros,
-        'libreria_seleccionada': libreria_seleccionada,
+        'librerias': list(librerias),
+        'info_libros': json.dumps(info_libros),
     }
     return render(request, 'libros/librerias_menu.html', context)
 
-def cargar_libros_por_libreria(request):
-    libreria_id = request.GET.get('libreria_id')
-    libros_qs = Libro.objects.filter(libreria_id=libreria_id)
 
-    info_libros = []
-    for libro in libros_qs:
-        try:
-            extension_scrap = importlib.import_module(f'libros.services.{libro.extension.nombre}.scrap')
-            libro_scrapped = extension_scrap.scrap_libro_details(libro)
-            info_libros.append({
-                'id': libro.id,
-                'titulo': libro.titulo,
-                'foto': libro_scrapped['foto'],
-                'libreria': libro.libreria_id
-            })
-        except Exception as e:
-            print(e)
-
-    return JsonResponse(info_libros, safe=False)
